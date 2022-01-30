0991b063cace0ecd9f7688e707ec2842ef496219
kolawole <akinde.kolawole@gmail.com>
2022-01-31 00:50:04 +0100

    latestUpdate

---
+++ app/src/main/java/com/ridehub360/ridepal/model/DestinationModel.kt
@@ -0,0 +1,5 @@
+package com.ridehub360.ridepal.model
+
+data class DestinationModel(val location: LocationModel,
+                            val duration: String,
+                            val distance: String)
